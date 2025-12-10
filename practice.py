9d0ac8863d1799d4cce67c13136f8caea4218e05
Venkata Swamy p <168516633+venkata200311@users.noreply.github.com>
2025-12-10 18:32:45 +0530

    Update practice.py

--- practice.py
+++ practice.py
@@ -1,8 +1,6 @@
-'''
 largest_num = lambda l1,k : sorted(set(l1))[-k] 
 
 print(largest_num([5,1,9,5,7,9],2))
-'''
 
 
 
@@ -14,41 +12,11 @@ print(largest_num([5,1,9,5,7,9],2))
 
 
 
-'''
+
 from functools import reduce
 k = lambda l1 : list(reduce(lambda n1,n2 : n1+n2 ,l1))
 
 print(k([[1,2],[3,4],[5]]))
-'''
-
-
-
-
-
-
-
-
-
-
-
-
-
-# l1 = [(1,3),(2,3),(4,5)]
-
-
-# # k = lambda l1 : sorted(l1,key=lambda x : x[1],reverse=True)
-# s = sorted([(1,3),(2,3),(4,5)],key=lambda x : l1[l1.index(x,0,len(l1))] if(l1.index(x,0,len(l1))+1==len(l1) and l1[l1.index(x,0,len(l1))]>l1[l1.index(x,0,len(l1))+1]) else l1[l1.index(x,0,len(l1))+1])
-
-# print(s)
-
-# # k = lambda l1 : reduce(lambda f,n : f if(f[1]>n[1]) else ((f if(f[0]>n[0]) else n) if(f[1]==n[1]) else n),l1)
-# # res = [k(l[i:])  for i in range(len(l)) ]
-
-# # print(res)
-# # print(k([(1,3),(2,3),(4,5)]))
-
-
-# res = filter()
 
 
 
@@ -63,13 +31,42 @@ print(k([[1,2],[3,4],[5]]))
 
 
 
+l1 = [(1,3),(2,3),(4,5)]
 
 
-'''
+# k = lambda l1 : sorted(l1,key=lambda x : x[1],reverse=True)
+s = sorted([(1,3),(2,3),(4,5)],key=lambda x : l1[l1.index(x,0,len(l1))] if(l1.index(x,0,len(l1))+1==len(l1) and l1[l1.index(x,0,len(l1))]>l1[l1.index(x,0,len(l1))+1]) else l1[l1.index(x,0,len(l1))+1])
+
+print(s)
+
+k = lambda l1 : reduce(lambda f,n : f if(f[1]>n[1]) else ((f if(f[0]>n[0]) else n) if(f[1]==n[1]) else n),l1)
+res = [k(l[i:])  for i in range(len(l)) ]
+
+print(res)
+print(k([(1,3),(2,3),(4,5)]))
+
+
+res = filter()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 k = lambda l1 : sorted(l1,key=lambda x : (-x[1],x[0]))
 
 print(k([(2,3),(1,3),(4,2),(1,5)]))
-'''
 
 
 
@@ -82,11 +79,11 @@ print(k([(2,3),(1,3),(4,2),(1,5)]))
 
 
 
-'''
+
+
 k = lambda s1 : "".join([i.lower() for i in s1 if(i.isalnum())])==("".join([i.lower() for i in s1 if(i.isalnum())]))[::-1]
 
 print(k("Race car"))
-'''
 
 
 
@@ -95,24 +92,12 @@ print(k("Race car"))
 
 
 
-'''
+
+
 s1 = "venkata"
 l1 = ["v","e","n","k","a","t","a"]
 print("".join(s1))
 print("".join(l1))
-'''
-
-
-
-
-
-
-
-
-
-# k = lambda s1 : {'vowels': i for i in range(0,len(s1)) if(s1[i] in "AEIOUaeiou")}
-
-# print(k("venkata"))
 
 
 
@@ -123,6 +108,9 @@ print("".join(l1))
 
 
 
+k = lambda s1 : {'vowels': i for i in range(0,len(s1)) if(s1[i] in "AEIOUaeiou")}
+
+print(k("venkata"))
 
 
 
@@ -136,7 +124,17 @@ print("".join(l1))
 
 
 
-'''
+
+
+
+
+
+
+
+
+
+
+
 marks = [90,85,70,35,40,55,63]
 
 def mark(m):
@@ -155,7 +153,7 @@ def mark(m):
 k = lambda m1 : list(filter(lambda ele : ele,mark(m1)))
 
 print(k(marks))
-'''
+
 
 
 
@@ -169,7 +167,7 @@ s= [("Ravi",45),("Anita",78),("Kiran",90),("Meena",32),("Arjun",55),("Lakshmi",8
 
 k = lambda l1 : dict(list(filter(lambda ele : ele[1]>35,l1)))
 
-# print(k(s))
+print(k(s))
 
 
 k1 = lambda l2 : {i[0].upper():i[1] for i in l2}
